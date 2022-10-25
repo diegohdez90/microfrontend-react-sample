6f08aed5669ebbd4c401e9dee92b73a1159360c4
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2022-10-25 12:12:20 -0500

    feat: init container

---
+++ packages/container/config/webpack.dev.js
@@ -0,0 +1,54 @@
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const common = require('./webpack.common');
+
+const dev = {
+	mode: 'development',
+	entry: "./src/index.ts",
+	resolve: {
+		extensions: ['.js', '.ts', '.tsx']
+	},
+	devServer: {
+		port: 8083,
+		historyApiFallback: true
+	},
+	module: {
+		rules: [{
+			test: /\.(scss)$/,
+			use: [{
+				loader: 'style-loader', // inject CSS to page
+			}, {
+				loader: 'css-loader', // translates CSS into CommonJS modules
+			}, {
+				loader: 'postcss-loader', // Run post css actions
+				options: {
+					postcssOptions: {
+						plugins: function () { // post css plugins, can be exported to postcss.config.js
+							return [
+								require('autoprefixer')
+							];
+						}
+					}
+				}
+			}, {
+				loader: 'sass-loader' // compiles Sass to CSS
+			}]
+		}, {
+			test: /\.svg$/i,
+			type: 'asset',
+			resourceQuery: /url/, // *.svg?url
+		}, {
+			test: /\.svg$/i,
+			issuer: /\.[jt]sx?$/,
+			resourceQuery: { not: [/url/] }, // exclude react component if *.svg?url
+			use: ['@svgr/webpack'],
+		}]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './public/index.html'
+		})
+	]
+};
+
+module.exports = merge(common, dev);
---
+++ packages/container/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+	return(<div>
+		<h4>App container</h4>
+	</div>);
+};
+
+export default App;
